180bdd1b86a1cad361158f5e78b5901498471d78
Dirk Groeneveld <dirkg@allenai.org>
2021-02-08 13:44:19 -0800

    Adds evaluation commands to run.sh

--- run.sh
+++ run.sh
@@ -9,3 +9,34 @@ allennlp train ./configs/temporal_bart_indexed_config.json -s ./temporal-bart-in
 wait -n
 wait -n
 
+# Evaluate both on CaTeRS
+allennlp evaluate ./temporal-bart-model ./released_data_models/data/eval/caters_entity_chains_seqdata_scrambled2.pkl --cuda-device 0 --include-package denoising_event_lm -o '{"validation_dataset_reader": {"chain_len_min": 2, "event_del_prob": 0.0, "event_del_all_prob": 0.0, "allow_empty_events": false}}'
+allennlp evaluate ./temporal-bart-indexed-model ./released_data_models/data/eval/caters_entity_chains_seqdata_scrambled2.pkl --cuda-device 0 --include-package denoising_event_lm -o '{"validation_dataset_reader": {"chain_len_min": 2, "event_del_prob": 0.0, "event_del_all_prob": 0.0, "allow_empty_events": false}}'
+
+# Evaluate both on MCTaco
+python3 denoising_event_lm/predictors/event_lm/test_demo_event_lm_mctaco_before_after.py \
+    --archive-path ./temporal-bart-model \
+    --predictor demo_denoising_event_lm_mctaco_before_after_predictor \
+    --include-package denoising_event_lm \
+    --cuda-device 0 \
+    -o '{}' --input-path ./released_data_models/data/eval/mctaco_event_ordering_before_after.json --beams 1 --feed-unseen > mctaco-eval.txt
+python3 denoising_event_lm/predictors/event_lm/test_demo_event_lm_mctaco_before_after.py \
+    --archive-path ./temporal-bart-indexed-model \
+    --predictor demo_denoising_event_lm_mctaco_before_after_predictor \
+    --include-package denoising_event_lm \
+    --cuda-device 0 \
+    -o '{}' --input-path ./released_data_models/data/eval/mctaco_event_ordering_before_after.json --beams 1 --feed-unseen > mctaco-eval-indexed.txt
+
+# Evaluate both on ordering unseen events
+python3 denoising_event_lm/predictors/event_lm/test_demo_event_lm_orderextra.py \
+    --archive-path ./temporal-bart-model/ \
+    --predictor demo_denoising_event_lm_orderextra_predictor \
+    --include-package denoising_event_lm \
+    --cuda-device 0 \
+    -o '{}' --input-path ./released_data_models/data/eval/caters_entity_chains_seqdata_scrambled2.pkl --beams 2 --chain_len_min 2 > unseen-eval.txt
+python3 denoising_event_lm/predictors/event_lm/test_demo_event_lm_orderextra.py \
+    --archive-path ./temporal-bart-indexed-model/ \
+    --predictor demo_denoising_event_lm_orderextra_predictor \
+    --include-package denoising_event_lm \
+    --cuda-device 0 \
+    -o '{}' --input-path ./released_data_models/data/eval/caters_entity_chains_seqdata_scrambled2.pkl --beams 2 --chain_len_min 2 > unseen-eval-indexed.txt
